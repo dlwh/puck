f2dc4791fd84e43bb26a85446e0774182b7a3486
David Hall <dlwh@cs.berkeley.edu>
2013-11-22 17:56:39 -0800

    improve the slice copy test a little

--- src/test/scala/puck/linalg/kernels/CLMatrixSliceCopyTest.scala
+++ src/test/scala/puck/linalg/kernels/CLMatrixSliceCopyTest.scala
@@ -36,10 +36,11 @@ class CLMatrixSliceCopyTest extends FunSuite {
     val mat2 = CLMatrix.zeros[Float](10,3)
     mat2 := -2.0f
     mat2(::, 1) := -3.0f
+    mat2(::, 2) := -0.0f
     val ev = sliceCopy.sliceCopyOut(mat(::, 0 until 3), Array(1, 2, 0), 3, mat2)
     ev.waitFor
     val dense = mat.toDense
-    assert(dense(::, 0).valuesIterator.forall(_ == -2.0f), dense)
+    assert(dense(::, 0).valuesIterator.forall(_ == -0.0f), dense)
     assert(dense(::, 1).valuesIterator.forall(_ == -2.0f), dense)
     assert(dense(::, 2).valuesIterator.forall(_ == -3.0f), dense)
   }
