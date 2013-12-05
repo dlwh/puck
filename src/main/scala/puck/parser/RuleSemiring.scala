515cd5dde01ea86f3efabf56dc6a52f145e70ef7
David Hall <dlwh@cs.berkeley.edu>
2013-12-05 14:50:00 -0800

    small cleanup to rulesemiring

--- src/main/scala/puck/parser/RuleSemiring.scala
+++ src/main/scala/puck/parser/RuleSemiring.scala
@@ -10,8 +10,6 @@ trait RuleSemiring extends Serializable {
 
   def fromLogSpace(float: Float): Float
   def mad(id: String, arg1: String, arg2: String):String
-
-
 }
 
 
@@ -28,10 +26,7 @@ object ViterbiRuleSemiring extends RuleSemiring {
 
   def plusIsIdempotent: Boolean = true
 
-  def zeroString = if(zero == Float.NegativeInfinity) "-INFINITY" else zero.toString
-
   def mad(id: String, arg1: String, arg2: String): String = {
-    //        s"float parent_${id}_pre_${arg1}_$x = parent_$id; parent_$id = max(parent_$id, $arg1 + $arg2); if(parent_$id > 0 &&  parent_${id}_pre_${arg1}_$x < 0) printf(${'"'}Rule ?!?!?! $id $arg1 $arg2 %f %f %f %f${"\\n"}${'"'}, parent_$id,  parent_${id}_pre_${arg1}_$x, $arg1, $arg2)"
     s"max($id, $arg1 + $arg2);"
   }
 
