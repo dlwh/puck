6c2c2c592540926075684fa2fd8d4b3a40e8d039
David Hall <dlwh@cs.berkeley.edu>
2014-01-02 20:08:27 -0600

    fix to buf ptr pair

--- src/main/scala/puck/util/CLBufferPointerPair.scala
+++ src/main/scala/puck/util/CLBufferPointerPair.scala
@@ -4,48 +4,13 @@ import com.nativelibs4java.opencl._
 import org.bridj.Pointer
 
 case class CLBufferPointerPair[V](val buffer: CLBuffer[V])(implicit queue: CLQueue) {
-  private var _mappedPointer:Pointer[V] = null
+  val ptr = Pointer.allocateArray(buffer.getIO, buffer.getElementCount)
   
-  def mappedPointer:Pointer[V] = mappedPointer()
-
-  def mappedPointer(events: CLEvent*): Pointer[V] = synchronized {
-    if(_mappedPointer eq null) {
-      _mappedPointer = buffer.map(queue, CLMem.MapFlags.ReadWrite, events:_*)
-    }
-    _mappedPointer
-  }
-
-  def unmap(evs: CLEvent*) = synchronized {
-    if(_mappedPointer ne null) { 
-      val ev = buffer.unmap(queue, _mappedPointer, evs:_*)
-      val mm = _mappedPointer
-      ev.invokeUponCompletion(new Runnable() {
-        def run() {  mm.release() }
-        })
-      _mappedPointer = null
-      ev
-    } else {
-      queue.enqueueWaitForEvents(evs:_*)
-      null
-    }
-  }
-
   def writeInts(dstOffset: Int, src: Array[Int], srcOffset: Int, len: Int, ev: CLEvent*)(implicit wit: Integer =:= V): CLEvent = {
-    val ptr = mappedPointer(ev:_*)
+    CLEvent.waitFor(ev:_*)
     ptr.setIntsAtOffset(dstOffset * 4, src, srcOffset, len)
-    unmap()
+    buffer.write(queue, dstOffset, len, ptr.offset(dstOffset * 4), false)
   }
-
-  def waitUnmap() {
-    Option(unmap()).foreach(_.waitFor)
-  }
-
-  def release() {
-    waitUnmap()
-    buffer.release()
-  }
-
-  def safeBuffer = { waitUnmap(); buffer }
 }
 
 
